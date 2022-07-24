f7a9c949f155348c8b99dd21e9b621544a352b3b
Ekaterina <kartyme0@gmail.com>
2022-07-24 23:57:10 +0300

    complete day8 tasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,18 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+         //String str = "";
+         StringBuilder sb = new StringBuilder();
+         for (int i = 0; i <= 20000; i++){
+             //str += Integer.toString(i);
+             sb.append(Integer.toString(i));
+             if (i != 20000){
+                 //str += " ";
+                 sb.append(" ");
+             }
+         }
+        //System.out.println(str);
+        System.out.println(sb);
 
     }
 }
